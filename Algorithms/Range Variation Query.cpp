201601fddcc00092536c5dff53f1dd6105db60c9
unknown <vinichenko.ia@students.dvfu.ru>
2020-03-01 13:04:36 +1000

    new

--- Algorithms/Range Variation Query.cpp
+++ Algorithms/Range Variation Query.cpp
@@ -1,80 +1,84 @@
 #include <iostream>
-#include <limits>
-#include <cmath>
-#include <cstdio>
+#include <stdio.h>
+#include <algorithm>
 #define MAX 100000
+#define INF 1e9
+
 using namespace std;
 
-long long tree [4 * MAX];
-
-
-void build (long long a[], int vertex, int tleft, int tright) {
-    if (tleft == tright)
-        tree[vertex] = 
-    else {
-        int tmiddle = (tleft + tright) / 2;
-        build (a, vertex * 2, tleft, tmiddle);
-        build (a, vertex * 2 + 1, tmiddle + 1, tright);
-        tree[vertex] = tree[vertex * 2] + tree[vertex * 2 + 1];
-    }
+struct node{
+  int minv, maxv;
 }
 
-void update (int vertex, int tleft, int tright, int position, int newvalue) {
-	if (tleft == tright)
-		tree[vertex] = newvalue;
-	else {
-		int tmiddle = (tleft + tright) / 2;
-		if (position <= tmiddle)
-			update (vertex * 2, tleft, tmiddle, position, newvalue);
-		else
-			update (vertex * 2 + 1, tmiddle + 1, tright, position, newvalue);
-		tree[vertex] = tree[vertex * 2] + tree[vertex * 2 + 1];
-	}
+SegmentedTree[4*MAX];
+
+void BuildTree(long long a[], int Vertex, int Left, int Right){
+  if (Left == Right)
+    SegmentedTree[Vertex].minv = SegmentedTree[Vertex].maxv = a[Left];
+  else{
+    int Middle = Left + (Right - Left) / 2;
+    BuildTree(a, 2*Vertex + 1, Left, Middle);
+    BuildTree(a, 2*Vertex + 2, Middle+1, Right);
+    SegmentedTree[Vertex].minv = min(SegmentedTree[2*Vertex + 1].minv,SegmentedTree[2*Vertex+2].minv);
+    SegmentedTree[Vertex].maxv = max(SegmentedTree[2*Vertex + 1].maxv,SegmentedTree[2*Vertex+2].maxv);
+      }
+
 }
 
-long long GetMin(int vertex, int tleft, int tright, int left, int right) {
-    if (left > right)
-        return std::numeric_limits<int>::max();
-    if (left == tleft && right == tright)
-        return tree[vertex];
-    int tmiddle = (tleft + tright) / 2;
-
-    return min(GetMin(vertex * 2, tleft, tmiddle, left, min(tmiddle, right)), GetMin(vertex * 2 + 1, tmiddle + 1, right, max(tmiddle + 1, left), right));
+long long GetMin(int Vertex, int LeftPosition, int RightPosition, int Left, int Right){
+  if (Left > Right)
+    return INF;
+  if ((Left == LeftPosition) && (Right == RightPosition))
+    return SegmentedTree[Vertex].minv;
+  int Middle = (LeftPosition + RightPosition) / 2;
+  return min(GetMin(2*Vertex + 1, LeftPosition, Middle, Left, min(Right,Middle)), GetMin(2*Vertex+2, Middle+1, RightPosition, max(Left,Middle+1),Right));
+}
+ 
+long long GetMax(int Vertex, int LeftPosition, int RightPosition, int Left, int Right){
+  if (Left > Right)
+    return -INF;
+  if ((Left == LeftPosition) && (Right == RightPosition))
+    return SegmentedTree[Vertex].maxv;
+  int Middle = (LeftPosition + RightPosition) / 2;
+  return max(GetMax(2*Vertex + 1, LeftPosition, Middle, Left, min(Right,Middle)), GetMax(2*Vertex+2, Middle+1, RightPosition, max(Left,Middle+1),Right));
 }
 
-long long GetMax(int vertex, int tleft, int tright, int left, int right){
-    if (left > right)
-        return -std::numeric_limits<int>::max();
-    if (left == tleft && right == tright)
-        return tree[vertex];
-    int tmiddle = (tleft + tright) / 2;
+void Update(int Vertex, int LeftPos, int RightPos, int Position, int NewValue){
+   if (LeftPos == RightPos)
+    SegmentedTree[Vertex].minv = SegmentedTree[Vertex].maxv = NewValue;
+  else{
+    int Middle = (LeftPos + RightPos) / 2;
+    if (Position <= Middle)
+      Update (2*Vertex + 1, LeftPos, Middle, Position, NewValue);
+    else
+      Update (2*Vertex+2, Middle+1, RightPos, Position, NewValue);
 
-    return max(GetMax(vertex * 2, tleft, tmiddle, left, min(tmiddle, right)), GetMax(vertex * 2 + 1, tmiddle + 1, right, max(tmiddle + 1, left), right));
+    SegmentedTree[Vertex].minv = min(SegmentedTree[2*Vertex + 1].minv,SegmentedTree[2*Vertex+2].minv);
+    SegmentedTree[Vertex].maxv = max(SegmentedTree[2*Vertex + 1].maxv,SegmentedTree[2*Vertex+2].maxv);
+     }
 }
 
 int main() {
 
-long long a[MAX];
-int k;
-int x, y;
-
 freopen("rvq.in", "r", stdin);
 freopen("rvq.out", "w", stdout);
 
-for (int i = 0; i <= MAX; i++)
-    a[i] = (i * i) % 12345 + ((i * i) % 23456 * i) % 23456;
+long long a[MAX + 1];
+int k;
+int x, y;
 
+for(int i = 0; i <= MAX; i++)
+  a[i] = (i * i) % 12345 + ((i * i) % 23456 * i) % 23456;
 
-build(a, 1, 0, MAX);
+BuildTree(a,1,0,MAX-1);
+
 cin >> k;
 
-//for (int i = 0; i < k; i++) {
-while (k--)
-{
-    cin >> x >> y;
-    if (x > 0)
-        cout << GetMax(1, 0, MAX, x, y) - GetMin(1, 0, MAX, x, y) << '\n';
-    else 
-        update(1, 0, MAX, -x, y);
-}
+while(k--){
+  cin >> x >> y;
+  if (x > 0) 
+    cout << GetMax(1,0,MAX-1,x,y) - GetMin(1,0,MAX-1,x,y) << '\n';
+  else
+    Update(1,0,MAX-1,-x,y);
+          }
 }
